b6e9eb9f37f8341d0ad8a6e3edb2b47905af1e64
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-01-16 19:43:41 +1100

    Enable query and rendering of images in 3 columns underneath form UI.

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -5,11 +5,32 @@ const Search = () => {
   const [query, setQuery] = useState("");
   const [images, setImages] = useState();
 
+  const queryImages = async (e) => {
+    e.preventDefault();
+    const apiEndpoint = "http://localhost:5000/images";
+    const apiUrl = new URL(apiEndpoint);
+    const queryParmaters = { query };
+    Object.keys(queryParmaters).forEach((key) =>
+      apiUrl.searchParams.append(key, queryParmaters[key])
+    );
+    const response = await fetch(
+      apiUrl,
+      process.env.NODE_ENV === "development" && { mode: "cors" }
+    );
+
+    if (response.ok) {
+      const responseInJson = await response.json();
+      setImages(responseInJson.response.results);
+    } else {
+      console.error({ response });
+    }
+  };
+
   return (
     <div className={css.search}>
-      <div>
+      <div className={css.container}>
         <h1 className={css.title}>Image Search Engine</h1>
-        <form onSubmit={() => alert("Images queried!")}>
+        <form onSubmit={(e) => queryImages(e)}>
           <label className={css.inputLabel}> 📷</label>
           <input
             type="text"
@@ -17,11 +38,25 @@ const Search = () => {
             placeholder="Search images"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
+            required
           />
           <button type="submit" className={css.button}>
             Search
           </button>
         </form>
+      </div>
+      <div className={css.card_list}>
+        {(images || []).map((image) => (
+          <div className={css.card}>
+            <img
+              className="card_image"
+              alt={image.alt_description}
+              src={image.urls.full}
+              width="50%"
+              height="50%"
+            ></img>
+          </div>
+        ))}
       </div>
     </div>
   );
